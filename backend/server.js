ad69289cbf18c17cea099a4a3edf9c031fd5c7b5
Mohit Sharma <sharmaamohit121@gmail.com>
2025-08-16 19:12:45 +0530

    moved to render from vercel and express version from 5 to 4

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,39 @@
 import express from 'express';
 import cors from 'cors';
-import "dotenv/config"
+import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 import authRoutes from './routes/authRoute.js';
-import taskRoutes from "./routes/taskRoute.js";
+import taskRoutes from './routes/taskRoute.js';
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173", 
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
-
-app.get('/ping', (_, res) => {
-  res.status(200).json({ message: 'pong' });
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-export default app;
+
+if (process.env.NODE_ENV === "production") {
+  const frontendDistPath = path.join(__dirname, '..', 'frontend', 'dist');
+  
+  app.use(express.static(frontendDistPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(frontendDistPath, "index.html"));
+  });
+}
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
